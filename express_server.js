ae92ed7f431f2efa014f753b33fbae4f420e7c83
Meghan Hein <meghan.hein@gmail.com>
2020-06-10 01:44:22 +0000

    created a logout that clears username cookie

--- express_server.js
+++ express_server.js
@@ -13,41 +13,59 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.redirect('/urls');
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.get("/", (req, res) => {
+  res.redirect('/urls');
+});
+
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
+});
+
+app.get("/u/urls/404", (req, res) => {
+  let templateVars = { username: req.cookies["username"] };
+  res.render("urls_404", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
   const newShort = generateRandomString()
   console.log(req.body)
-  const newLong = Object.values(req.body)
+  const newLong = req.body.longURL
   urlDatabase[newShort] = 'http://' + newLong;
   res.redirect(`urls/${newShort}`);
-
 });
 
+app.post("/login", (req, res) => {
+  const username = req.body.username
+  res.cookie("username", username)
+  res.redirect("/urls")
+})
 
-app.get("/u/urls/404", (req, res) => {
-  res.render("urls_404");
-});
+app.post("/logout", (req, res) => {
+  const username = req.cookies.username
+  res.clearCookie("username", username)
+  res.redirect("/urls")
+})
+
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
